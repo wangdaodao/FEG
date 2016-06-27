a29f8ffb2b05a9af6e408c95b84fc62249770b79
wangdaodao <hi@wangdaodao.com>
2016-06-27 16:36:13 +0800

    update sprite

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@
 使用原始
 npm config set registry = "https://registry.npmjs.org/"
 使用淘宝
-npm config set registry = "https://registry.npm.taobao.org/"
-
-npm install gulp node-sass gulp-sass gulp-autoprefixer gulp-minify-css gulp-livereload gulp-uglify gulp-webserver gulp-concat gulp-clean gulp-zip gulp-plumber opn --save-dev
+npm config set registry = "http://registry.npm.taobao.org/"
+npm install gulp node-sass gulp-sass gulp-autoprefixer gulp-minify-css gulp-livereload gulp-uglify gulp-webserver gulp-concat gulp-clean gulp-zip gulp-plumber gulp.spritesmith opn --save-dev
 */
 var gulp         = require('gulp'),
     sass         = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
+    spritesmith  = require('gulp.spritesmith'),
     minifycss    = require('gulp-minify-css'),
     livereload   = require('gulp-livereload'),
     uglify       = require('gulp-uglify'),
@@ -19,17 +19,18 @@ var gulp         = require('gulp'),
     plumber      = require('gulp-plumber'),
     opn          = require('opn');
 
-//配置本地Web 服务器：主机+端口
-var localserver = {
-  host: 'localhost',
-  port: '8080'
-}
+// 说明
+gulp.task('help',function () {
+  console.log(' gulp debug     调试任务');
+  console.log(' gulp watch     文件监控');
+  console.log(' gulp build     文件打包');
+  console.log(' gulp help      帮助');
+});
 
 //删除js文件
 gulp.task('cleanjs', function () {
-  var stream = gulp.src('./js/all.js')
+  return gulp.src('./js/all.js')
     .pipe(clean());
-  return stream;
 });
 
 //合并javascript文件，合并后文件放入js下按顺序压缩gulp.src(['a.js', 'b.js', 'c.js'])
@@ -48,12 +49,40 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./css'));
 });
 
+// 合并雪碧图
+gulp.task('sprite', function () {
+  return gulp.src('img/sprite/*')//需要合并的图片地址
+    .pipe(spritesmith({
+        imgName: '../img/sprite.png',//保存合并后图片的地址
+        cssName: '../css/_sprite.scss',//保存合并后对于css样式的地址
+        cssFormat: 'scss',
+        padding:5,//合并时两个图片的间距
+        algorithm: 'binary-tree',//注释1
+        cssTemplate: function (data) {
+          var arr=[];
+          data.sprites.forEach(function (sprite) {
+              arr.push(".icon-"+sprite.name+
+              "{" +
+              "background-image: url('"+sprite.escaped_image+"');"+
+              "background-position: "+sprite.px.offset_x+" "+sprite.px.offset_y+";"+
+              "width:"+sprite.px.width+";"+
+              "height:"+sprite.px.height+";"+
+              "}\n");
+          });
+          return arr.join("");
+        }
+    }))
+    .pipe(gulp.dest('./img'));
+});
+
 //文件监控
 gulp.task('watch', function () {
+  // Watch sprite files
+  gulp.watch('img/sprite/**', ['sprite']);
   // Watch .scss files
-  gulp.watch('./css/*.scss', ['styles']);
+  gulp.watch('css/**', ['styles']);
   // Watch .js files
-  gulp.watch('./js/*.js', ['alljs']);
+  gulp.watch('js/**', ['alljs']);
 });
 
 //调试监控
@@ -66,6 +95,11 @@ gulp.task('debugwatch', function () {
   });
 });
 
+//配置本地Web 服务器：主机+端口
+var localserver = {
+  host: 'localhost',
+  port: '8080'
+}
 //开启本地 Web 服务器功能
 gulp.task('webserver', function() {
   gulp.src( './' )
@@ -76,51 +110,46 @@ gulp.task('webserver', function() {
       directoryListing: false
     }));
 });
-
 //通过浏览器打开本地 Web服务器 路径
 gulp.task('openbrowser', function() {
-    opn( 'http://' + localserver.host + ':' + localserver.port );
+  opn( 'http://' + localserver.host + ':' + localserver.port );
 });
 
 //把HTML拷贝到build下 
 gulp.task('buildhtml', function() {
-  var stream = gulp.src('./*.html')
+  return gulp.src('./*.html')
     .pipe(gulp.dest('./build'));
-  return stream;
 });
 
 //把CSS拷贝到build下
 gulp.task('buildcss', ['styles'] , function() {
-  var stream = gulp.src('./css/*.css')
+  return gulp.src('./css/*.css')
     .pipe(minifycss())
     .pipe(gulp.dest('./build/css'));
-  return stream;
 });
 
 //把IMG拷贝到build下
 gulp.task('buildimg', function() {
-  var stream = gulp.src('./img/**')
+  return gulp.src('./img/**')
     .pipe(gulp.dest('./build/img'));
-  return stream;
 });
 
 //把PLUGIN拷贝到build下
 gulp.task('buildplugin', function() {
-  var stream = gulp.src('./plugin/**')
+  return gulp.src('./plugin/**')
     .pipe(gulp.dest('./build/plugin'));
-  return stream;
 });
 
 //把JS拷贝到build下
 gulp.task('buildjs', ['alljs'] , function() {
-  var stream = gulp.src('./js/all.js')
+  return gulp.src('./js/all.js')
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
-  return stream;
 });
 
 //默认任务 gulp start
 gulp.task('start', function(){
+  gulp.start('sprite');
   gulp.start('styles');
   gulp.start('alljs');
   gulp.start('watch');
@@ -130,6 +159,7 @@ gulp.task('start', function(){
 
 //调试任务 gulp debug
 gulp.task('debug', function(){
+  gulp.start('sprite');
   gulp.start('styles');
   gulp.start('alljs');
   gulp.start('watch');
